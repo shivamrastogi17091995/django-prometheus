a226197ec6a969009cb38662609789c2381331a4
Shivam Rastogi <shivamrastogi17091995@gmail.com>
2020-10-17 01:04:08 +0530

    Changes

--- django_prometheus/middleware.py
+++ django_prometheus/middleware.py
@@ -1,7 +1,6 @@
 from prometheus_client import Counter, Histogram
 
 from django.conf import settings
-from django.utils.deprecation import MiddlewareMixin
 from django_prometheus.utils import PowersOf, Time, TimeSince
 
 DEFAULT_LATENCY_BUCKETS = (
@@ -24,6 +23,20 @@ DEFAULT_LATENCY_BUCKETS = (
     float("inf"),
 )
 
+class MiddlewareMixin(object):
+    def __init__(self, get_response=None):
+        self.get_response = get_response
+        super(MiddlewareMixin, self).__init__()
+
+    def __call__(self, request):
+        response = None
+        if hasattr(self, 'process_request'):
+            response = self.process_request(request)
+        if not response:
+            response = self.get_response(request)
+        if hasattr(self, 'process_response'):
+            response = self.process_response(request, response)
+        return response
 
 class Metrics(object):
     _instance = None
